d60bd190abcb89597c286eaaca8a895fb390b25e
liuderchi <liuderchi@gmail.com>
2018-05-13 15:06:20 +0800

    server index.html

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 var app = require('express')();
 var http = require('http').Server(app);
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function(){
+http.listen(3000, function() {
   console.log('listening on *:3000');
 });
