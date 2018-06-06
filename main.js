84eca6dd93a396f608cbf9439784d47d0731b528
Te-Chi Liu <liuderchi@gmail.com>
2018-06-06 23:24:43 +0800

    feat(main): fetch weather API with CORS anyweather
    - NOTE: replace with your own key
    - NOTE: can be broken down

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
-function update() {
+
+const CORS_ANYWHERE_DOMAIN = 'https://cors-anywhere.herokuapp.com';
+
+const darkSkyAPI = {
+  url: 'https://api.darksky.net/forecast',
+  key: '501c815982d69b200afd4b1671befbd8',
+};
+
+const openWeatherMapAPI = {
+  url: 'http://api.openweathermap.org/data/2.5/weather',
+  query: {
+    q: '',
+    appid: 'c50accef3c5022b54a605268032345fd',
+  },
+};
+
+function f2c(f) {
+  return Math.ceil((f - 32) * 5 / 9 * 10) / 10;
+}
+
+async function update() {
   // Toggle refresh state
   $('#update .icon').toggleClass('d-none');
 
+  openWeatherMapAPI.query.q = $('#city').val() || 'Taipei';
+  if (!$('#city').val()) {
+    $('#city').val(openWeatherMapAPI.query.q);
+  }
   $('#date').text(
     `, ${new Date()
       .toDateString()
@@ -11,7 +35,29 @@ function update() {
   );
 
   try {
-    // TODO fetch weather data
+    const res = await fetch(
+      `${CORS_ANYWHERE_DOMAIN}/${openWeatherMapAPI.url}?` +
+        `q=${openWeatherMapAPI.query.q}&` +
+        `appid=${openWeatherMapAPI.query.appid}`,
+    ).then(data => data.json());
+
+    console.warn(`fetching weather data in ${res.name}...`);
+
+    // NOTE add Allow all origins in reponse by hitting cors-anywhere proxy
+    const weatherData = await fetch(
+      `${CORS_ANYWHERE_DOMAIN}/${darkSkyAPI.url}/` +
+        `${darkSkyAPI.key}/${res.coord.lat},${res.coord.lon}`,
+    ).then(data => data.json());
+
+    console.warn({ weatherData });
+
+    $('h4#info').html(`
+      <span class="large">${f2c(
+        weatherData.currently.apparentTemperature,
+      )}</span> &deg;C
+      &emsp;<span class="large">${weatherData.currently.precipProbability *
+        100}</span> %
+    `);
 
     // TODO draw chart
 
