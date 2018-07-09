9d1c1b176a48091ea0380c1b4a579e26cb457df7
Te-Chi Liu <liuderchi@gmail.com>
2018-07-09 11:51:23 +0800

    [4] simple nodejs http server
    - callback style for listen
    - event with callback for request coming
    - you can set env in start script (NOT start:watch)

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello World')
+const http = require('http');
+
+const requestHandler = (request, response) => {
+  console.log(request.url);
+  response.end('Hello from NodeJS Server!');
+};
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, () => {
+  console.log(`Server listening at port ${port}`);
+});
