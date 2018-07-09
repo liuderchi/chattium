8c7135fdba557a1df43eb92c1c4daed1ccc1aae8
Te-Chi Liu <liuderchi@gmail.com>
2018-07-09 11:56:09 +0800

    [5] npm i -S express, express
    - express() returns an app

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
+const express = require('express');
+const path = require('path');
 const http = require('http');
-
-const requestHandler = (request, response) => {
-  console.log(request.url);
-  response.end('Hello from NodeJS Server!');
-};
 
 const port = process.env.PORT || 3000;
 
-const server = http.createServer(requestHandler);
+const app = express();
+const server = http.createServer(app);
 
 server.listen(port, () => {
   console.log(`Server listening at port ${port}`);
