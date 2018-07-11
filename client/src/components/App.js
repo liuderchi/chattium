1eaa168e4a8daabc5ac951739aab7ea4b35c050b
Te-Chi Liu <liuderchi@gmail.com>
2018-07-11 21:29:52 +0800

    [2] add < Welcome />
    - simple comp: functional comp
    - className to pass css class
    - argument as prop
    - component is a module
    - state: vs prop

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import logo from '../icons/logo.svg';
+import Welcome from './Welcome';
 import './App.css';
 
 class App extends Component {
+  state = {
+    numUsers: 0,
+  };
   render() {
+    const { numUsers } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Welcome numUsers={numUsers} />
       </div>
     );
   }
